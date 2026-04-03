e98a136fc1f8d2ead8f6ac11e17c3ab80609e5ac
MIKE <187060323+mtq01@users.noreply.github.com>
2026-04-03 00:48:59 -0700

    updated readme and added another function to functions.php

--- functions.php
+++ functions.php
@@ -35,12 +35,64 @@ function bs_register_block_styles()
 
         - only load the CSS if a 'glow' paragraph block is used on the page  */
     wp_enqueue_block_style('core/paragraph', array(
-        'handle' => 'bs-glow-style',
+        'handle' => 'bs-style-glow',
         'src' => get_theme_file_uri('assets/css/blocks/paragraph-glow.css'),
         'path' => get_theme_file_path('assets/css/blocks/paragraph-glow.css')
     ));
 }
 add_action('init', 'bs_register_block_styles');
+
+
+
+
+
+// ------------------------------------------------------------------------------------------------------------
+// [universal loader]
+// 'bs_register_custom_block_styles_universal()' replaces 'bs_register_block_styles' / 'register_block_style()' 
+// function. used if loading more than a couple different block styles. 
+// [note]: scans '/styles/blocks/ for .json files and registers them automatically.
+
+/*
+function bs_register_custom_block_styles_universal() {
+    $directory = get_theme_file_path('/styles/blocks/');
+
+    if ( is_dir( $directory ) ) {
+        $files = glob( $directory . '*.json' );
+
+        foreach ( $files as $file ) {
+            $slug = basename( $file, '.json' );
+            
+            // dismantle file name. ex: change "paragraph-glow" to get "paragraph" as the block_name
+            // for this to work, your file name needs to start with its block type.
+            // otherwise "core/$block_name" below won't register properly.
+            $parts = explode('-', $slug);
+            $block_name = $parts[0]; 
+            $style_name = $parts[1];
+
+            // register style in UI
+            register_block_style(
+                "core/$block_name",
+                array(
+                    'name'  => $style_name,
+                    'label' => ucwords($style_name),
+                    'is_default' => false,
+                )
+            );
+
+            // enqueue the specific CSS file for this style (only if its used on a page)
+            wp_enqueue_block_style("core/$block_name", array(
+                'handle' => "bs-style-$slug",
+                'src'    => get_theme_file_uri("/assets/css/blocks/$slug.css"),
+                'path'   => get_theme_file_path("/assets/css/blocks/$slug.css"),
+            ));
+        }
+    }
+}
+add_action('init', 'bs_register_custom_block_styles_universal');
+
+*/
+
+// ------------------------------------------------------------------------------------------------------------
 
 
 
